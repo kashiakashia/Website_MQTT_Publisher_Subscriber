df4370d4f7018b45757d85841e9b07fea1767d95
kashiakashia <37773120+kashiakashia@users.noreply.github.com>
2024-03-22 12:45:56 +0100

    fixed issue with disconnect function

--- src/script.js
+++ src/script.js
@@ -1,4 +1,37 @@
-function ConnectToBroker() {
+//<input type="button" class="button" onclick="ConnectToBroker()" value="Connect">
+let client;
+let con = document.querySelector("#connect");
+con.addEventListener("click", connectToBroker);
+
+let dis = document.querySelector("#disconnect");
+dis.addEventListener("click", disconnectFromBroker);
+
+function connectClient(options) {
+  client = mqtt.connect(options.host, options);
+
+  document.getElementById("message-mqtt").innerHTML +=
+    "<span>Connecting to " + host + " on port " + port + "</span> <br/>";
+
+  document.getElementById("message-mqtt").innerHTML +=
+    "<span>Using the client ID " + client.options.clientId + "</span> <br/>";
+
+  client.on("connect", function () {
+    console.log("Client connected");
+  });
+
+  topic = document.getElementById("topic-subscriber").value;
+  document.getElementById("message-mqtt").innerHTML +=
+    "<span>Subscribed to topic " + topic + "</span> <br/>";
+  client.subscribe(topic);
+
+  client.on("message", function (topic, message) {
+    // message is Buffer
+    console.log("sub: " + message.toString());
+  });
+}
+
+function connectToBroker(event) {
+  event.preventDefault();
   host = "wws://" + document.getElementById("host").value;
   port = document.getElementById("port").value;
   userID = document.getElementById("username").value;
@@ -14,36 +47,21 @@ function ConnectToBroker() {
     password: passwordID,
   };
 
-  const client = mqtt.connect(host, options);
-
-  document.getElementById("message-mqtt").innerHTML +=
-    "<span>Connecting to " + host + " on port " + port + "</span> <br/>";
-
-  document.getElementById("message-mqtt").innerHTML +=
-    "<span>Using the client ID " + client.options.clientId + "</span> <br/>";
-
-  client.on("connect", function () {
-    console.log("Client connected");
-  });
-
-  //-------------------------
-
-  topic = document.getElementById("topic-subscriber").value;
-  document.getElementById("message-mqtt").innerHTML +=
-    "<span>Subscribed to topic " + topic + "</span> <br/>";
-  client.subscribe(topic);
-
-  client.on("message", function (topic, message) {
-    // message is Buffer
-    console.log("sub: " + message.toString());
-  });
-
-  return client;
+  connectClient(options);
 }
 
-function DisconnectFromBroker(client) {
-  client.end(true);
-  console.log(
-    "Client with ID: " + client.options.clientId + ", is disconnected"
-  );
+function disconnectFromBroker(event) {
+  event.preventDefault();
+  console.log("diconnecting...");
+  stopConnection(client);
+}
+
+function stopConnection(client) {
+  setTimeout(function () {
+    //clearInterval(pubLoop);
+    client.end();
+    console.log(
+      "Client with ID: " + client.options.clientId + ", is disconnected"
+    );
+  }, 10); // stop after 0.0001sec
 }
